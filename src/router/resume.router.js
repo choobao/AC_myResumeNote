36be81e9f9686e1e8d77e37cd44d2307b0bccdcf
choobao <154485888+choobao@users.noreply.github.com>
2024-01-31 12:42:24 +0900

    내 이력서 목록 조회 api

--- src/router/resume.router.js
+++ src/router/resume.router.js
@@ -4,7 +4,7 @@ import { prisma } from "../utils/prisma/index.js";
 
 const router = express.Router();
 
-//내 이력서 생성 api(+)
+//내 이력서 생성 api
 router.post("/resumes", authMiddleWare, async (req, res, next) => {
   const { userId } = req.user;
   const { title, content } = req.body; //status는 자동으로 apply로 등록
@@ -19,21 +19,19 @@ router.post("/resumes", authMiddleWare, async (req, res, next) => {
   return res.status(201).json({ data: resume });
 });
 
-//나의 이력서 목록 조회(-)
+//나의 이력서 목록 조회
 router.get("/resumes", authMiddleWare, async (req, res, next) => {
   const { userId } = req.user;
   //로그인 된 사용자의 아이디를 프리즈마의 users 필드와 비교해
   //본인이 쓴 이력서만 내림차순으로 조회
-  const resumes = await prisma.users.findMany({
+  const resumes = await prisma.resumes.findMany({
     where: { userId: +userId },
-    Resumes: {
-      select: {
-        resumeId: true,
-        title: true,
-        createdAt: true,
-        updatedAt: true,
-        //상세조회에서 content 조회가능하도록
-      },
+    select: {
+      resumeId: true,
+      title: true,
+      createdAt: true,
+      updatedAt: true,
+      //상세조회에서 content 조회가능하도록
     },
     orderBy: {
       createdAt: "desc",
